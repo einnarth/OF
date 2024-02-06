0f215001f738b884222579f095bfda09e08dd455
samuelmalik <113421307+samuelmalik@users.noreply.github.com>
2024-02-06 08:49:45 +0100

    add logo to navigation bar, clear home component and add css for home component

--- OF/ClientApp/src/app/home/home.component.ts
+++ OF/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthorizeService } from '../../api-authorization/authorize.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public isAuthenticated?: Observable<boolean>;
+  public userName?: Observable<string | null | undefined>;
+
+  constructor(private authorizeService: AuthorizeService) { }
+
+  ngOnInit() {
+    this.isAuthenticated = this.authorizeService.isAuthenticated();
+    this.userName = this.authorizeService.getUser().pipe(map(u => u && u.name));
+  }
 }
